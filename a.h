37cb72dc6e0affd82e3a59bba2d5a78cde2b21ac
shuangyh <876260018@qq.com>
2022-07-06 10:27:42 +0800

    add node

--- a.h
+++ a.h
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
+
+struct Node
+{
+    string country;
+    string name;
+    string state;
+    uint32_t id;
+    /* data */
+};
+
+
 int main()
 {
+    Node node;
+    node.country = "China";
     int a = 0;
     a = 5;
 	cout<<"Hello world!\n";
